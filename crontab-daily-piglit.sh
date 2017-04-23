1add030e28433d2d3dfba2faed4dc527a0002a4d
Andres Gomez <agomez@igalia.com>
2017-04-24 00:50:22 +0300

    cron: Initial daily job

---
+++ crontab-daily-piglit.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+PATH=${HOME}/.local/bin$(echo :$PATH | sed -e s@:${HOME}/.local/bin@@g)
+
+# PATH to the mesa-dockerfiles.git repository
+MESA_DOCKER_FILES=$HOME/mesa-dockerfiles.git
+# PATH where to place the piglit results
+PIGLIT_RESULTS=$HOME/i965/piglit-results
+
+function cleanup {
+    if [ "x${CONTAINER}" == "x" ]; then
+	docker rm ${CONTAINER}
+    fi
+
+    exit $1
+}
+
+if [ -d "${MESA_DOCKER_FILES}" ]; then
+    cd "${MESA_DOCKER_FILES}"
+else
+    cleanup 1
+fi
+
+git pull -q
+
+dj -q -d Dockerfile.piglit.jinja -o Dockerfile -e RELEASE=17.0 -e MAKEFLAGS=-j2
+CONTAINER=$(docker build -q --pull -f Dockerfile -t igalia/mesa:piglit .)
+rm Dockerfile
+
+if [ -d "${PIGLIT_RESULTS}" ]; then
+    docker run --privileged --rm -t -v "${PIGLIT_RESULTS}":/results:Z  -e DISPLAY=unix$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix igalia/mesa:piglit
+else
+    cleanup 2
+fi
+
+cleanup 0
