1be2fc1ff72292fa85d07741a6fb40ea2cfd8683
Andres Gomez <agomez@igalia.com>
2017-04-26 23:32:23 +0300

    piglit: quieted the crontab and adapted to ccache
    Since we added ccache, the piglit dockerfile was failing.

--- crontab-daily-piglit.sh
+++ crontab-daily-piglit.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
+# Paths
+# -----
+
+# PATH to the mesa-dockerfiles.git repository
+CDP_MESA_DOCKERFILES_DIR="${CDP_MESA_DOCKERFILES_DIR:-$HOME/mesa-dockerfiles.git}"
+# PATH where to place the piglit results
+CDP_PIGLIT_RESULTS_DIR="${CDP_PIGLIT_RESULTS_DIR:-$HOME/i965/piglit-results}"
+# PATH where for ccache's directory
+CDP_DOCKER_CCACHE_DIR="${CDP_DOCKER_CCACHE_DIR:-$HOME/i965/piglit-results/docker-ccache}"
+
+# Verbose?
+# --------
+
+CDP_VERBOSE="${CDP_VERBOSE:-false}"
+
 usage()
 {
     echo -e "\e[31mUSAGE:"
@@ -19,34 +34,58 @@ PATH=${HOME}/.local/bin$(echo :$PATH | sed -e s@:${HOME}/.local/bin@@g)
 DISPLAY="${DISPLAY:-:0.0}"
 export -p DISPLAY
 
-# PATH to the mesa-dockerfiles.git repository
-MESA_DOCKER_FILES="${MESA_DOCKER_FILES:-$HOME/mesa-dockerfiles.git}"
-# PATH where to place the piglit results
-PIGLIT_RESULTS="${PIGLIT_RESULTS:-$HOME/i965/piglit-results}"
+if ${CDP_VERBOSE}; then
+    CDP_OUTPUT=1
+    CDP_QUIET=""
+else
+    CDP_OUTPUT=/dev/null
+    CDP_QUIET="-q"
+fi
 
 function cleanup {
-    if [ "x${CONTAINER}" == "x" ]; then
-	docker rm ${CONTAINER}
+    if [ "x$1" == "x0" ]; then
+        docker rmi image-piglit >&"${CDP_OUTPUT}" 2>&1
     fi
 
     exit $1
 }
 
-if [ -d "${MESA_DOCKER_FILES}" ]; then
-    cd "${MESA_DOCKER_FILES}"
+if [ -d "${CDP_MESA_DOCKERFILES_DIR}" ]; then
+    cd "${CDP_MESA_DOCKERFILES_DIR}"
 else
+    echo "${CDP_MESA_DOCKERFILES_DIR} directory doesn't exist."
     cleanup 1
 fi
 
-git pull -q
+if [ ! -d "${CDP_PIGLIT_RESULTS_DIR}" ]; then
+    echo "${CDP_PIGLIT_RESULTS_DIR} directory doesn't exist."
+    cleanup 2
+fi
 
-dj -q -d Dockerfile.piglit.jinja -o Dockerfile -e RELEASE="${RELEASE}" -e MAKEFLAGS=-j2
-CONTAINER=$(docker build -q --pull -f Dockerfile -t igalia/mesa:piglit .)
+if [ ! -d "${CDP_DOCKER_CCACHE_DIR}" ]; then
+    echo "${CDP_DOCKER_CCACHE_DIR} directory doesn't exist."
+    cleanup 3
+fi
+
+git pull ${CDP_QUIET}
+
+dj ${CDP_QUIET} -d Dockerfile.piglit.jinja -o Dockerfile -e RELEASE="${RELEASE}" -e MAKEFLAGS=-j2
+docker build ${CDP_QUIET} --pull -f Dockerfile -t image-piglit .
 rm Dockerfile
+docker run -v "${CDP_DOCKER_CCACHE_DIR}":/home/local/.ccache \
+       -v "${CDP_PIGLIT_RESULTS_DIR}":/results:Z \
+       --name container-piglit image-piglit \
+       "/bin/sh" "-c" "cmake . && make" >&"${CDP_OUTPUT}" 2>&1
+docker commit container-piglit image-piglit >&"${CDP_OUTPUT}" 2>&1
+docker rm container-piglit >&"${CDP_OUTPUT}" 2>&1
 
-if [ -d "${PIGLIT_RESULTS}" ]; then
+if [ -d "${CDP_PIGLIT_RESULTS_DIR}" ]; then
     for i in i965 llvmpipe swr softpipe; do
-	docker run --privileged --rm -t -v "${PIGLIT_RESULTS}":/results:Z  -e DISPLAY=unix$DISPLAY -e GL_DRIVER=$i -v /tmp/.X11-unix:/tmp/.X11-unix igalia/mesa:piglit
+	docker run --privileged --rm -t -v "${CDP_PIGLIT_RESULTS_DIR}":/results:Z \
+	       -e DISPLAY=unix$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix \
+	       -e FPR_VERBOSE=$CDP_VERBOSE \
+	       -e GL_DRIVER=$i image-piglit \
+	       "/bin/sh" "-c" "cat /home/local/mesa-head.txt >> /results/mesa-head.txt && cat /home/local/piglit-head.txt >> /results/piglit-head.txt && export MESA_COMMIT=\$(grep commit /home/local/mesa-head.txt | cut -d \" \" -f 2) && export FPR_CREATE_PIGLIT_REPORT=true && export FPR_RUN_PIGLIT=true && export FPR_PIGLIT_PATH=/home/local/piglit && export FPR_PIGLIT_REPORTS_PATH=/results && /home/local/full-piglit-run.sh \$GL_DRIVER \$MESA_COMMIT"
     done
 else
     cleanup 2
