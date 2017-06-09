30df38bea60dc3f1b60df47ba82fe668e60f391a
Andres Gomez <agomez@igalia.com>
2017-06-10 01:52:23 +0300

    cron: replace --verbose with --verbosity level

--- crontab-daily-piglit.sh
+++ crontab-daily-piglit.sh
@@ -2,6 +2,14 @@
 #
 #		Runs piglit test on a given set of mesa3D drivers
 #
+# This script is intended to run automatically as a cronjob, making
+# use of full-piglit-run.sh
+#
+# Example:
+#
+# $ crontab -e
+# ...
+# 0 3 * * * bash <path_to>/mesa-dockerfiles.git/crontab-daily-piglit.sh --run-piglit --run-vk-cts --run-gl-cts --release pre-release-17.1 i965
 
 export LC_ALL=C
 
@@ -17,18 +25,22 @@ export MAKEFLAGS
 #			Function: check_verbosity
 #------------------------------------------------------------------------------
 #
-# check if quiet and verbose are both passed:
+# perform sanity check on the passed verbosity level:
+#   $1 - the verbosity to use
 # returns:
-#   if it returns, everything is good
-#   otherwise it exit's
-check_verbosity() {
-    if ${CDP_QUIET:-false} && ${CDP_VERBOSE:-false}; then
-	echo ""
-	echo "The --quiet and --verbose flags are incompatible"
-	echo ""
-	usage
-	exit 1
-    fi
+#   0 is success, an error code otherwise
+function check_verbosity() {
+    case "x$1" in
+	"xfull" | "xnormal" | "xquiet" )
+	    ;;
+	*)
+	    printf "%s\n" "Error: Only verbosity levels among [full|normal|quiet] are allowed." >&2
+	    usage
+	    return 1
+	    ;;
+    esac
+
+    return 0
 }
 
 #------------------------------------------------------------------------------
@@ -36,16 +48,18 @@ check_verbosity() {
 #------------------------------------------------------------------------------
 #
 check_local_changes() {
-    git diff --quiet HEAD > /dev/null 2>&1
+    git diff --quiet HEAD
     if [ $? -ne 0 ]; then
-	echo ""
-	echo "Uncommitted changes found. Did you forget to commit? Aborting."
-	echo ""
-	echo "You can perform a 'git stash' to save your local changes and"
-	echo "a 'git stash apply' to recover them afterwards."
-	echo ""
+	printf "%s\n" \
+	       "" \
+	       "Uncommitted changes found. Did you forget to commit? Aborting." \
+	       "" \
+	       "You can perform a 'git stash' to save your local changes and" \
+	       "a 'git stash apply' to recover them afterwards." \
+	       "" >&2
 	return 1
     fi
+
     return 0
 }
 
@@ -66,9 +80,7 @@ check_option_args() {
 
     # check for an argument
     if [ x"$arg" = x ]; then
-	echo ""
-	echo "Error: the '$option' option is missing its required argument."
-	echo ""
+	printf "%s\n" "Error: the '$option' option is missing its required argument." >&2
 	usage
 	exit 1
     fi
@@ -76,9 +88,7 @@ check_option_args() {
     # does the argument look like an option?
     echo $arg | $CDP_GREP "^-" > /dev/null
     if [ $? -eq 0 ]; then
-	echo ""
-	echo "Error: the argument '$arg' of option '$option' looks like an option itself."
-	echo ""
+	printf "%s\n" "Error: the argument '$arg' of option '$option' looks like an option itself." >&2
 	usage
 	exit 1
     fi
@@ -96,10 +106,10 @@ function cleanup {
     if [ "x$1" == "x0" ]; then
 	if $CDP_CLEAN; then
 	    if $CDP_RUN_PIGLIT; then
-		docker rmi igalia/mesa:piglit >&"${CDP_OUTPUT}" 2>&1
+		docker rmi igalia/mesa:piglit
 	    fi
 	    if $CDP_RUN_VK_CTS || $CDP_RUN_GL_CTS; then
-		docker rmi igalia/mesa:vk-gl-cts >&"${CDP_OUTPUT}" 2>&1
+		docker rmi igalia/mesa:vk-gl-cts
 	    fi
 	fi
     fi
@@ -116,13 +126,9 @@ function cleanup {
 # returns:
 #   0 is success, an error code otherwise
 function header {
-    if ! $CDP_QUIET; then
-	TIMESTAMP=`date +%Y%m%d%H%M%S`
-	echo "Running $1 at $TIMESTAMP"
-	echo ""
-	df -h
-	echo ""
-    fi
+    CDP_TIMESTAMP=`date +%Y%m%d%H%M%S`
+    CDP_SPACE=$(df -h)
+    printf "%s\n" "Running $1 at $CDP_TIMESTAMP" "" "$CDP_SPACE" "" >&2
 
     return 0
 }
@@ -168,17 +174,13 @@ function test_suites {
 #   0 is success, an error code otherwise
 function run_piglit_tests {
     if [ "${CDP_RELEASE:-x}" == "x" ]; then
-	echo ""
-	echo "A release has to be provided."
-	echo ""
+	printf "%s\n" "Error: a release has to be provided." >&2
 	usage
 	return 1
     fi
 
     if [ "${CDP_MESA_DRIVERS:-x}" == "x" ]; then
-	echo ""
-	echo "At least one mesa3d driver must be passed."
-	echo ""
+	printf "%s\n" "Error: at least one mesa3d driver must be passed." >&2
 	usage
 	return 2
     fi
@@ -186,23 +188,17 @@ function run_piglit_tests {
     if [ -d "${CDP_MESA_DOCKERFILES_DIR}" ]; then
 	cd "${CDP_MESA_DOCKERFILES_DIR}"
     else
-	echo ""
-	echo "${CDP_MESA_DOCKERFILES_DIR} directory doesn't exist."
-	echo ""
+	printf "%s\n" "Error: ${CDP_MESA_DOCKERFILES_DIR} directory doesn't exist." >&2
 	return 3
     fi
 
     if [ ! -d "${CDP_PIGLIT_RESULTS_DIR}" ]; then
-	echo ""
-	echo "${CDP_PIGLIT_RESULTS_DIR} directory doesn't exist."
-	echo ""
+	printf "%s\n" "Error: ${CDP_PIGLIT_RESULTS_DIR} directory doesn't exist." >&2
 	return 4
     fi
 
     if [ ! -d "${CCACHE_DIR}" ]; then
-	echo ""
-	echo "${CCACHE_DIR} directory doesn't exist."
-	echo ""
+	printf "%s\n" "Error: ${CCACHE_DIR} directory doesn't exist." >&2
 	return 5
     fi
 
@@ -211,42 +207,34 @@ function run_piglit_tests {
 	return 6
     fi
 
-    git pull ${CDP_SILENCE}
+    git pull
 
 
     if $CDP_RUN_PIGLIT; then
-	rocker build --pull -f Rockerfile.piglit --var TAG=piglit --var RELEASE="${CDP_RELEASE}" >&"${CDP_OUTPUT}" 2>&1
+	rocker build --pull -f Rockerfile.piglit --var TAG=piglit --var RELEASE="${CDP_RELEASE}"
 	CDP_TEST_SUITES="piglit $CDP_TEST_SUITES"
     fi
 
     if $CDP_RUN_GL_CTS || $CDP_RUN_VK_CTS; then
 	cp Rockerfile.vk-gl-cts $HOME
 	cd $HOME/LoaderAndValidationLayers
-	git pull ${CDP_SILENCE}
-	cd - > /dev/null
+	git pull
+	cd -
 	cd $HOME/vk-gl-cts
-	git pull ${CDP_SILENCE}
-	cd - > /dev/null
+	git pull
+	cd -
 	cd $HOME
-	rocker build --pull -f Rockerfile.vk-gl-cts --var VIDEO_GID=`getent group video | cut -f3 -d:` --var TAG=vk-gl-cts --var RELEASE="${CDP_RELEASE}" >&"${CDP_OUTPUT}" 2>&1
+	rocker build --pull -f Rockerfile.vk-gl-cts --var VIDEO_GID=`getent group video | cut -f3 -d:` --var TAG=vk-gl-cts --var RELEASE="${CDP_RELEASE}"
 	rm Rockerfile.vk-gl-cts
-	cd - > /dev/null
+	cd -
     fi
 
-    if $CDP_VERBOSE; then
-	CDP_EXTRA_ARGS="--verbosity full $CDP_EXTRA_ARGS"
-    elif $CDP_QUIET; then
-	CDP_EXTRA_ARGS="--verbosity quiet $CDP_EXTRA_ARGS"
-    fi
+    CDP_EXTRA_ARGS="--verbosity $CDP_VERBOSITY $CDP_EXTRA_ARGS"
 
     for suite in $(test_suites); do
 	for driver in $CDP_MESA_DRIVERS; do
 	    corrected_driver=`proper_driver "$suite" "$driver"`
-	    if ! $CDP_QUIET; then
-		echo ""
-		echo "Processing $suite test suite with driver $corrected_driver ..."
-		echo ""
-	    fi
+	    printf "%s\n" "" "Processing $suite test suite with driver $corrected_driver ..." "" >&2
 	    if ! $CDP_DRY_RUN; then
 		if [ "x$suite" = "xpiglit" ]; then
 		    docker run --privileged --rm -t -v "${CDP_PIGLIT_RESULTS_DIR}":/results:Z \
@@ -282,15 +270,14 @@ usage() {
     basename="`expr "//$0" : '.*/\([^/]*\)'`"
     cat <<HELP
 
-Usage: $basename [options] --release <release> path...
+Usage: $basename [options] <driver>
 
-Where "path" is a relative path to a git module, including '.'.
+Where "driver" is one of [i965|anv|llvmpipe|swr|softpipe]
 
 Options:
   --dry-run               Does everything except running the tests
   --no-clean              Do not clean the created image
-  --quiet                 Be quiet
-  --verbose               Be verbose
+  --verbosity             Which verbosity level to use [full|normal|quite]. Default, normal.
   --help                  Display this help and exit successfully
   --mesa-dockerfiles-dir  PATH to the mesa-dockerfiles.git repository
   --piglit-results-dir    PATH where to place the piglit results
@@ -328,15 +315,11 @@ do
     --no-clean)
 	CDP_CLEAN=false
 	;;
-    # Be quiet
-    --quiet)
-	CDP_QUIET=true
-	check_verbosity
-	;;
-    # Be verbose
-    --verbose)
-	CDP_VERBOSE=true
-	check_verbosity
+    # Which verbosity level to use [full|normal|quite]. Default, normal.
+    --verbosity)
+	check_option_args $1 $2
+	shift
+	export CDP_VERBOSITY=$1
 	;;
     # Display this help and exit successfully
     --help)
@@ -380,16 +363,12 @@ do
 	CDP_RUN_PIGLIT=true
 	;;
     --*)
-	echo ""
-	echo "Error: unknown option: $1"
-	echo ""
+	printf "%s\n" "Error: unknown option: $1" >&2
 	usage
 	exit 1
 	;;
     -*)
-	echo ""
-	echo "Error: unknown option: $1"
-	echo ""
+	printf "%s\n" "Error: unknown option: $1" >&2
 	usage
 	exit 1
 	;;
@@ -418,15 +397,23 @@ CDP_RUN_VK_CTS="${CDP_RUN_VK_CTS:-false}"
 CDP_RUN_GL_CTS="${CDP_RUN_GL_CTS:-false}"
 CDP_RUN_PIGLIT="${CDP_RUN_PIGLIT:-false}"
 
-# Quiet?
-# ------
-
-CDP_QUIET="${CDP_QUIET:-false}"
-
 # Verbose?
 # --------
 
-CDP_VERBOSE="${CDP_VERBOSE:-false}"
+CDP_VERBOSITY="${CDP_VERBOSITY:-normal}"
+
+check_verbosity $CDP_VERBOSITY
+if [ $? -ne 0 ]; then
+    exit 13
+fi
+
+if [ "x$CDP_VERBOSITY" != "xfull" ]; then
+    exec > /dev/null
+fi
+
+if [ "x$CDP_VERBOSITY" == "xquiet" ]; then
+    exec 2>&1
+fi
 
 # dry run?
 # --------
@@ -439,14 +426,6 @@ CDP_DRY_RUN="${CDP_DRY_RUN:-false}"
 CDP_CLEAN="${CDP_CLEAN:-true}"
 
 # ---
-
-if ${CDP_VERBOSE}; then
-    CDP_OUTPUT=1
-    CDP_SILENCE=""
-else
-    CDP_OUTPUT=/dev/null
-    CDP_SILENCE="-q"
-fi
 
 header $0
 
