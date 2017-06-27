386f6058b5f1baabf6bd26df5d0c7707eed354a7
Andres Gomez <agomez@igalia.com>
2017-06-27 16:31:21 +0300

    cron: replace CFPR prefix with CDLP

--- crontab-daily-local-piglit.sh
+++ crontab-daily-local-piglit.sh
@@ -111,7 +111,7 @@ function check_option_args() {
     fi
 
     # does the argument look like an option?
-    echo $arg | $CFPR_GREP "^-" > /dev/null
+    echo $arg | $CDLP_GREP "^-" > /dev/null
     if [ $? -eq 0 ]; then
 	printf "Error: the argument '$arg' of option '$option' looks like an option itself.\n" >&2
 	usage
@@ -137,23 +137,23 @@ function sanity_check() {
 	return 2
     fi
 
-    pushd "$CFPR_MESA_PATH"
+    pushd "$CDLP_MESA_PATH"
     git fetch origin
     git show -s --pretty=format:%h "$1" > /dev/null
-    CFPR_RESULT=$?
+    CDLP_RESULT=$?
     popd
-    if [ $CFPR_RESULT -ne 0 ]; then
+    if [ $CDLP_RESULT -ne 0 ]; then
 	printf "%s\n" "" "Error: mesa's commit id doesn't exist in the repository." "" >&2
 	usage
 	return 3
     fi
 
-    pushd "$CFPR_VK_GL_CTS_PATH"
+    pushd "$CDLP_VK_GL_CTS_PATH"
     git fetch origin
     git show -s --pretty=format:%h "$2" > /dev/null
-    CFPR_RESULT=$?
+    CDLP_RESULT=$?
     popd
-    if [ $CFPR_RESULT -ne 0 ]; then
+    if [ $CDLP_RESULT -ne 0 ]; then
 	printf "%s\n" "" "Error: VK-GL-CTS' commit id doesn't exist in the repository." "" >&2
 	usage
 	return 4
@@ -172,9 +172,9 @@ function sanity_check() {
 # returns:
 #   0 is success, an error code otherwise
 function header {
-    CFPR_TIMESTAMP=$(date +%Y%m%d%H%M%S)
-    CFPR_SPACE=$(df -h)
-    printf "%s\n" "Running $1 at $CFPR_TIMESTAMP" "" "$CFPR_SPACE" "" >&9
+    CDLP_TIMESTAMP=$(date +%Y%m%d%H%M%S)
+    CDLP_SPACE=$(df -h)
+    printf "%s\n" "Running $1 at $CDLP_TIMESTAMP" "" "$CDLP_SPACE" "" >&9
 
     return 0
 }
@@ -191,27 +191,27 @@ function header {
 # returns:
 #   0 is success, an error code otherwise
 function build_mesa() {
-    rm -rf "$CFPR_TEMP_PATH/mesa"
-    git clone "$CFPR_MESA_PATH" "$CFPR_TEMP_PATH/mesa"
-    pushd "$CFPR_MESA_PATH"
-    CFPR_ORIGIN_URL=$(git remote get-url origin)
+    rm -rf "$CDLP_TEMP_PATH/mesa"
+    git clone "$CDLP_MESA_PATH" "$CDLP_TEMP_PATH/mesa"
+    pushd "$CDLP_MESA_PATH"
+    CDLP_ORIGIN_URL=$(git remote get-url origin)
     popd
-    pushd "$CFPR_TEMP_PATH/mesa"
-    git remote set-url origin "$CFPR_ORIGIN_URL"
+    pushd "$CDLP_TEMP_PATH/mesa"
+    git remote set-url origin "$CDLP_ORIGIN_URL"
     git fetch origin
     git branch -m old
     if $1; then
-	COMMIT=$(git merge-base origin/master "$CFPR_MESA_BRANCH")
+	COMMIT=$(git merge-base origin/master "$CDLP_MESA_BRANCH")
     else
-	COMMIT="$CFPR_MESA_BRANCH"
+	COMMIT="$CDLP_MESA_BRANCH"
     fi
     git checkout -b working "$COMMIT"
     git branch -D old
-    CFPR_MESA_COMMIT=$(git show -s --pretty=format:%h "$COMMIT")
+    CDLP_MESA_COMMIT=$(git show -s --pretty=format:%h "$COMMIT")
     wget https://raw.githubusercontent.com/Igalia/mesa-dockerfiles/master/Rockerfile.mesa
     # make check is failing right now and we don't really need it
     sed -e 's/&& make check//g' -i Rockerfile.mesa
-    rocker build -f Rockerfile.mesa --var BUILD="autotools" --var LLVM="3.9" --var DEBUG=true --var TAG=released-17.1.2."$CFPR_MESA_COMMIT"
+    rocker build -f Rockerfile.mesa --var BUILD="autotools" --var LLVM="3.9" --var DEBUG=true --var TAG=released-17.1.2."$CDLP_MESA_COMMIT"
     popd
 
     return 0
@@ -226,9 +226,9 @@ function build_mesa() {
 # returns:
 #   0 is success, an error code otherwise
 function clean_mesa() {
-    rm -rf "$CFPR_TEMP_PATH/mesa"
-    if $CFPR_CLEAN; then
-	docker rmi igalia/mesa:released-17.1.2."$CFPR_MESA_COMMIT"
+    rm -rf "$CDLP_TEMP_PATH/mesa"
+    if $CDLP_CLEAN; then
+	docker rmi igalia/mesa:released-17.1.2."$CDLP_MESA_COMMIT"
     fi
 
     return 0
@@ -243,34 +243,34 @@ function clean_mesa() {
 # returns:
 #   0 is success, an error code otherwise
 function build_vk_gl_cts() {
-    rm -rf "$CFPR_TEMP_PATH/LoaderAndValidationLayers"
-    git clone "$CFPR_VK_LOADER_PATH" "$CFPR_TEMP_PATH/LoaderAndValidationLayers"
-    pushd "$CFPR_VK_LOADER_PATH"
-    CFPR_ORIGIN_URL=$(git remote get-url origin)
+    rm -rf "$CDLP_TEMP_PATH/LoaderAndValidationLayers"
+    git clone "$CDLP_VK_LOADER_PATH" "$CDLP_TEMP_PATH/LoaderAndValidationLayers"
+    pushd "$CDLP_VK_LOADER_PATH"
+    CDLP_ORIGIN_URL=$(git remote get-url origin)
     popd
-    pushd "$CFPR_TEMP_PATH/LoaderAndValidationLayers"
-    git remote set-url origin "$CFPR_ORIGIN_URL"
+    pushd "$CDLP_TEMP_PATH/LoaderAndValidationLayers"
+    git remote set-url origin "$CDLP_ORIGIN_URL"
     git fetch origin
     git branch -m old
     git checkout -b working origin/master
     git branch -D old
     popd
 
-    rm -rf "$CFPR_TEMP_PATH/vk-gl-cts"
-    git clone "$CFPR_VK_GL_CTS_PATH" "$CFPR_TEMP_PATH/vk-gl-cts"
-    pushd "$CFPR_VK_GL_CTS_PATH"
-    CFPR_ORIGIN_URL=$(git remote get-url origin)
+    rm -rf "$CDLP_TEMP_PATH/vk-gl-cts"
+    git clone "$CDLP_VK_GL_CTS_PATH" "$CDLP_TEMP_PATH/vk-gl-cts"
+    pushd "$CDLP_VK_GL_CTS_PATH"
+    CDLP_ORIGIN_URL=$(git remote get-url origin)
     popd
-    pushd "$CFPR_TEMP_PATH/vk-gl-cts"
-    git remote set-url origin "$CFPR_ORIGIN_URL"
+    pushd "$CDLP_TEMP_PATH/vk-gl-cts"
+    git remote set-url origin "$CDLP_ORIGIN_URL"
     git fetch origin
     git branch -m old
-    git checkout -b working "$CFPR_VK_GL_CTS_BRANCH"
+    git checkout -b working "$CDLP_VK_GL_CTS_BRANCH"
     git branch -D old
     popd
-    pushd "$CFPR_TEMP_PATH"
+    pushd "$CDLP_TEMP_PATH"
     wget https://raw.githubusercontent.com/Igalia/mesa-dockerfiles/master/Rockerfile.vk-gl-cts
-    rocker build -f Rockerfile.vk-gl-cts --var VIDEO_GID=`getent group video | cut -f3 -d:` --var TAG=vk-gl-cts --var RELEASE=released-17.1.2."$CFPR_MESA_COMMIT"
+    rocker build -f Rockerfile.vk-gl-cts --var VIDEO_GID=`getent group video | cut -f3 -d:` --var TAG=vk-gl-cts --var RELEASE=released-17.1.2."$CDLP_MESA_COMMIT"
     popd
 
     return 0
@@ -285,9 +285,9 @@ function build_vk_gl_cts() {
 # returns:
 #   0 is success, an error code otherwise
 function clean_vk_gl_cts() {
-    rm -rf "$CFPR_TEMP_PATH/vk-gl-cts"
-    rm -f "$CFPR_TEMP_PATH/Rockerfile.vk-gl-cts"
-    if $CFPR_CLEAN; then
+    rm -rf "$CDLP_TEMP_PATH/vk-gl-cts"
+    rm -f "$CDLP_TEMP_PATH/Rockerfile.vk-gl-cts"
+    if $CDLP_CLEAN; then
 	docker rmi igalia/mesa:vk-gl-cts
     fi
 
@@ -303,11 +303,11 @@ function clean_vk_gl_cts() {
 # returns:
 #   0 is success, an error code otherwise
 function build_piglit() {
-    rm -rf "$CFPR_TEMP_PATH/piglit"
-    mkdir -p "$CFPR_TEMP_PATH/piglit"
-    pushd "$CFPR_TEMP_PATH/piglit"
+    rm -rf "$CDLP_TEMP_PATH/piglit"
+    mkdir -p "$CDLP_TEMP_PATH/piglit"
+    pushd "$CDLP_TEMP_PATH/piglit"
     wget https://raw.githubusercontent.com/Igalia/mesa-dockerfiles/master/Rockerfile.piglit
-    rocker build -f Rockerfile.piglit --var TAG=piglit --var RELEASE=released-17.1.2."$CFPR_MESA_COMMIT"
+    rocker build -f Rockerfile.piglit --var TAG=piglit --var RELEASE=released-17.1.2."$CDLP_MESA_COMMIT"
     popd
 
     return 0
@@ -322,8 +322,8 @@ function build_piglit() {
 # returns:
 #   0 is success, an error code otherwise
 function clean_piglit() {
-    rm -rf "$CFPR_TEMP_PATH/piglit"
-    if $CFPR_CLEAN; then
+    rm -rf "$CDLP_TEMP_PATH/piglit"
+    if $CDLP_CLEAN; then
 	docker rmi igalia/mesa:piglit
     fi
 
@@ -344,7 +344,7 @@ function create_piglit_reference() {
 	return -1
     fi
 
-    ln -sfr $(ls -d $CFPR_PIGLIT_RESULTS_DIR/results/all-"$1"* | tail -1) -T $CFPR_PIGLIT_RESULTS_DIR/reference/all-"$1"
+    ln -sfr $(ls -d $CDLP_PIGLIT_RESULTS_DIR/results/all-"$1"* | tail -1) -T $CDLP_PIGLIT_RESULTS_DIR/reference/all-"$1"
 
     return 0
 }
@@ -363,7 +363,7 @@ function create_gl_cts_reference() {
 	return -1
     fi
 
-    ln -sfr $(ls -d $CFPR_PIGLIT_RESULTS_DIR/results/GL-CTS-"$1"* | tail -1) -T $CFPR_PIGLIT_RESULTS_DIR/reference/GL-CTS-"$1"
+    ln -sfr $(ls -d $CDLP_PIGLIT_RESULTS_DIR/results/GL-CTS-"$1"* | tail -1) -T $CDLP_PIGLIT_RESULTS_DIR/reference/GL-CTS-"$1"
 
     return 0
 }
@@ -382,7 +382,7 @@ function create_vk_cts_reference() {
 	return -1
     fi
 
-    ln -sfr $(ls -d $CFPR_PIGLIT_RESULTS_DIR/results/VK-CTS-"$1"* | tail -1) -T $CFPR_PIGLIT_RESULTS_DIR/reference/VK-CTS-"$1"
+    ln -sfr $(ls -d $CDLP_PIGLIT_RESULTS_DIR/results/VK-CTS-"$1"* | tail -1) -T $CDLP_PIGLIT_RESULTS_DIR/reference/VK-CTS-"$1"
 
     return 0
 }
@@ -397,78 +397,78 @@ function create_vk_cts_reference() {
 #   0 is success, an error code otherwise
 function run_tests {
 
-    if [ ! -d "${CFPR_PIGLIT_RESULTS_DIR}" ]; then
-	printf "%s\n" "Error: ${CFPR_PIGLIT_RESULTS_DIR} directory doesn't exist." >&2
+    if [ ! -d "${CDLP_PIGLIT_RESULTS_DIR}" ]; then
+	printf "%s\n" "Error: ${CDLP_PIGLIT_RESULTS_DIR} directory doesn't exist." >&2
 	return 4
     fi
 
     header
 
-    mkdir -p "$CFPR_TEMP_PATH/jail"
+    mkdir -p "$CDLP_TEMP_PATH/jail"
 
-    pushd "$CFPR_TEMP_PATH/jail"
+    pushd "$CDLP_TEMP_PATH/jail"
 
-    build_mesa $CFPR_MERGE_BASE_RUN
+    build_mesa $CDLP_MERGE_BASE_RUN
 
-    CFPR_EXTRA_ARGS="--verbosity $CFPR_VERBOSITY $CFPR_EXTRA_ARGS"
-    $CFPR_CREATE_PIGLIT_REPORT && CFPR_EXTRA_ARGS="--create-piglit-report $CFPR_EXTRA_ARGS"
+    CDLP_EXTRA_ARGS="--verbosity $CDLP_VERBOSITY $CDLP_EXTRA_ARGS"
+    $CDLP_CREATE_PIGLIT_REPORT && CDLP_EXTRA_ARGS="--create-piglit-report $CDLP_EXTRA_ARGS"
 
-    if $CFPR_RUN_PIGLIT; then
+    if $CDLP_RUN_PIGLIT; then
     	build_piglit
 
 	# We restore the redirection so the output is managed by the
 	# commands inside "docker run"
 	restore_redirection
 
-	docker run --privileged --rm -t -v "${CFPR_PIGLIT_RESULTS_DIR}":/results:Z \
+	docker run --privileged --rm -t -v "${CDLP_PIGLIT_RESULTS_DIR}":/results:Z \
 	       -e DISPLAY=unix$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix \
-	       -e FPR_EXTRA_ARGS="$CFPR_EXTRA_ARGS" \
+	       -e FPR_EXTRA_ARGS="$CDLP_EXTRA_ARGS" \
 	       -e GL_DRIVER="i965" igalia/mesa:piglit
 
-	apply_verbosity "$CFPR_VERBOSITY"
+	apply_verbosity "$CDLP_VERBOSITY"
 
-	$CFPR_MERGE_BASE_RUN && create_piglit_reference "i965"
+	$CDLP_MERGE_BASE_RUN && create_piglit_reference "i965"
 
     	clean_piglit
     fi
 
-    if $CFPR_RUN_GL_CTS || $CFPR_RUN_VK_CTS; then
+    if $CDLP_RUN_GL_CTS || $CDLP_RUN_VK_CTS; then
     	build_vk_gl_cts
 
-	if $CFPR_RUN_GL_CTS; then
+	if $CDLP_RUN_GL_CTS; then
     	    printf "%s\n" "" "Checking GL CTS progress ..." "" >&9
 
 	    # We restore the redirection so the output is managed by
 	    # the commands inside "docker run"
 	    restore_redirection
 
-	    docker run --privileged --rm -t -v "${CFPR_PIGLIT_RESULTS_DIR}":/results:Z \
+	    docker run --privileged --rm -t -v "${CDLP_PIGLIT_RESULTS_DIR}":/results:Z \
 		   -e DISPLAY=unix$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix \
-		   -e FPR_EXTRA_ARGS="$CFPR_EXTRA_ARGS" \
+		   -e FPR_EXTRA_ARGS="$CDLP_EXTRA_ARGS" \
 		   -e CTS="opengl" \
 		   -e GL_DRIVER="i965" igalia/mesa:vk-gl-cts
 
-	    apply_verbosity "$CFPR_VERBOSITY"
+	    apply_verbosity "$CDLP_VERBOSITY"
 
-	    $CFPR_MERGE_BASE_RUN && create_gl_cts_reference "i965"
+	    $CDLP_MERGE_BASE_RUN && create_gl_cts_reference "i965"
 	fi
 
-	if $CFPR_RUN_VK_CTS; then
+	if $CDLP_RUN_VK_CTS; then
     	    printf "%s\n" "" "Checking VK CTS progress ..." "" >&9
 
 	    # We restore the redirection so the output is managed by
 	    # the commands inside "docker run"
 	    restore_redirection
 
-	    docker run --privileged --rm -t -v "${CFPR_PIGLIT_RESULTS_DIR}":/results:Z \
+	    docker run --privileged --rm -t -v "${CDLP_PIGLIT_RESULTS_DIR}":/results:Z \
 		   -e DISPLAY=unix$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix \
-		   -e FPR_EXTRA_ARGS="--vk-cts-all-concurrent $CFPR_EXTRA_ARGS" \
+		   -e FPR_EXTRA_ARGS="--vk-cts-all-concurrent $CDLP_EXTRA_ARGS" \
 		   -e CTS="vulkan" \
 		   -e GL_DRIVER="anv" igalia/mesa:vk-gl-cts
 
-	    apply_verbosity "$CFPR_VERBOSITY"
+	    apply_verbosity "$CDLP_VERBOSITY"
 
-	    $CFPR_MERGE_BASE_RUN && create_vk_cts_reference "anv"
+	    $CDLP_MERGE_BASE_RUN && create_vk_cts_reference "anv"
 	fi
 
     	clean_vk_gl_cts
@@ -477,7 +477,7 @@ function run_tests {
     clean_mesa
 
     popd
-    rm -rf "$CFPR_TEMP_PATH/"
+    rm -rf "$CDLP_TEMP_PATH/"
 
     return 0
 }
@@ -525,9 +525,9 @@ HELP
 # Choose which grep program to use (on Solaris, must be gnu grep)
 if [ "x$CDP_GREP" = "x" ] ; then
     if [ -x /usr/gnu/bin/grep ] ; then
-	CFPR_GREP=/usr/gnu/bin/grep
+	CDLP_GREP=/usr/gnu/bin/grep
     else
-	CFPR_GREP=grep
+	CDLP_GREP=grep
     fi
 fi
 
@@ -545,83 +545,83 @@ do
     --verbosity)
 	check_option_args $1 $2
 	shift
-	CFPR_VERBOSITY=$1
+	CDLP_VERBOSITY=$1
 	;;
     # Do not clean the created images
     --no-clean)
-	CFPR_CLEAN=false
+	CDLP_CLEAN=false
 	;;
     # Forces the cleaning of the working env
     --force-clean)
-	CFPR_FORCE_CLEAN=true
+	CDLP_FORCE_CLEAN=true
 	;;
     # PATH from which to create the rest of the relative paths
     --base-path)
 	check_option_args $1 $2
 	shift
-	CFPR_BASE_PATH=$1
+	CDLP_BASE_PATH=$1
 	;;
     # PATH in which to do the temporary work
     --tmp-path)
 	check_option_args $1 $2
 	shift
-	CFPR_TEMP_PATH=$1
+	CDLP_TEMP_PATH=$1
 	;;
     # PATH to the mesa repository
     --mesa-path)
 	check_option_args $1 $2
 	shift
-	CFPR_MESA_PATH=$1
+	CDLP_MESA_PATH=$1
 	;;
     # PATH to the vk-gl-cts repository
     --vk-gl-cts-path)
 	check_option_args $1 $2
 	shift
-	CFPR_VK_GL_CTS_PATH=$1
+	CDLP_VK_GL_CTS_PATH=$1
 	;;
     # PATH to the LoaderAndValidationLayers repository
     --vk-loader-path)
 	check_option_args $1 $2
 	shift
-	CFPR_VK_LOADER_PATH=$1
+	CDLP_VK_LOADER_PATH=$1
 	;;
     # PATH where to place the piglit results
     --piglit-results-dir)
 	check_option_args $1 $2
 	shift
-	CFPR_PIGLIT_RESULTS_DIR=$1
+	CDLP_PIGLIT_RESULTS_DIR=$1
 	;;
     # mesa commit to use
     --mesa-commit)
 	check_option_args $1 $2
 	shift
-	CFPR_MESA_BRANCH=$1
+	CDLP_MESA_BRANCH=$1
 	;;
     # VK-GL-CTS commit to use
     --vk-gl-cts-commit)
 	check_option_args $1 $2
 	shift
-	CFPR_VK_GL_CTS_BRANCH=$1
+	CDLP_VK_GL_CTS_BRANCH=$1
 	;;
     # merge-base run
     --merge-base-run)
-	CFPR_MERGE_BASE_RUN=true
+	CDLP_MERGE_BASE_RUN=true
 	;;
     # Run vk-cts
     --run-vk-cts)
-	CFPR_RUN_VK_CTS=true
+	CDLP_RUN_VK_CTS=true
 	;;
     # Run gl-cts
     --run-gl-cts)
-	CFPR_RUN_GL_CTS=true
+	CDLP_RUN_GL_CTS=true
 	;;
     # Run piglit
     --run-piglit)
-	CFPR_RUN_PIGLIT=true
+	CDLP_RUN_PIGLIT=true
 	;;
     # Create results report
     --create-piglit-report)
-	CFPR_CREATE_PIGLIT_REPORT=true
+	CDLP_CREATE_PIGLIT_REPORT=true
 	;;
     --*)
 	printf "Error: unknown option: $1\n" >&2
@@ -647,52 +647,52 @@ done
 # Paths ...
 # ---------
 
-CFPR_BASE_PATH="${CFPR_BASE_PATH:-$HOME/i965}"
-CFPR_TEMP_PATH="${CFPR_TEMP_PATH:-$CFPR_BASE_PATH/cfpr-temp}"
-CFPR_MESA_PATH="${CFPR_MESA_PATH:-$CFPR_BASE_PATH/mesa.git}"
-CFPR_VK_GL_CTS_PATH="${CFPR_VK_GL_CTS_PATH:-$CFPR_BASE_PATH/vk-gl-cts.git}"
-CFPR_VK_LOADER_PATH="${CFPR_VK_LOADER_PATH:-$CFPR_BASE_PATH/LoaderAndValidationLayers.git}"
+CDLP_BASE_PATH="${CDLP_BASE_PATH:-$HOME/i965}"
+CDLP_TEMP_PATH="${CDLP_TEMP_PATH:-$CDLP_BASE_PATH/cfpr-temp}"
+CDLP_MESA_PATH="${CDLP_MESA_PATH:-$CDLP_BASE_PATH/mesa.git}"
+CDLP_VK_GL_CTS_PATH="${CDLP_VK_GL_CTS_PATH:-$CDLP_BASE_PATH/vk-gl-cts.git}"
+CDLP_VK_LOADER_PATH="${CDLP_VK_LOADER_PATH:-$CDLP_BASE_PATH/LoaderAndValidationLayers.git}"
 # PATH where to place the piglit results
-CFPR_PIGLIT_RESULTS_DIR="${CFPR_PIGLIT_RESULTS_DIR:-$CFPR_BASE_PATH/piglit-results}"
+CDLP_PIGLIT_RESULTS_DIR="${CDLP_PIGLIT_RESULTS_DIR:-$CDLP_BASE_PATH/piglit-results}"
 
 
 # merge-base run?
 # ---------------
 
-CFPR_MERGE_BASE_RUN="${CFPR_MERGE_BASE_RUN:-false}"
+CDLP_MERGE_BASE_RUN="${CDLP_MERGE_BASE_RUN:-false}"
 
 
 # What tests to run?
 # ------------------
 
-CFPR_RUN_VK_CTS="${CFPR_RUN_VK_CTS:-false}"
-CFPR_RUN_GL_CTS="${CFPR_RUN_GL_CTS:-false}"
-CFPR_RUN_PIGLIT="${CFPR_RUN_PIGLIT:-false}"
+CDLP_RUN_VK_CTS="${CDLP_RUN_VK_CTS:-false}"
+CDLP_RUN_GL_CTS="${CDLP_RUN_GL_CTS:-false}"
+CDLP_RUN_PIGLIT="${CDLP_RUN_PIGLIT:-false}"
 
 
 # Cleaning?
 # ---------
 
-CFPR_CLEAN="${CFPR_CLEAN:-true}"
+CDLP_CLEAN="${CDLP_CLEAN:-true}"
 
 
 # Create a report against the reference result?
 # ---------------------------------------------
 
-CFPR_CREATE_PIGLIT_REPORT="${CFPR_CREATE_PIGLIT_REPORT:-false}"
+CDLP_CREATE_PIGLIT_REPORT="${CDLP_CREATE_PIGLIT_REPORT:-false}"
 
 
 # Force clean
 # ------------
 
-CFPR_FORCE_CLEAN="${CFPR_FORCE_CLEAN:-false}"
+CDLP_FORCE_CLEAN="${CDLP_FORCE_CLEAN:-false}"
 
-if $CFPR_FORCE_CLEAN; then
+if $CDLP_FORCE_CLEAN; then
     clean_mesa
     clean_piglit
     clean_vk_gl_cts
-    printf "%s\n" "" "rm -Ir $CFPR_TEMP_PATH" ""
-    rm -Ir "$CFPR_TEMP_PATH"
+    printf "%s\n" "" "rm -Ir $CDLP_TEMP_PATH" ""
+    rm -Ir "$CDLP_TEMP_PATH"
 
     exit 0
 fi
@@ -701,9 +701,9 @@ fi
 # Verbosity level
 # ---------------
 
-CFPR_VERBOSITY="${CFPR_VERBOSITY:-normal}"
+CDLP_VERBOSITY="${CDLP_VERBOSITY:-normal}"
 
-check_verbosity "$CFPR_VERBOSITY"
+check_verbosity "$CDLP_VERBOSITY"
 if [ $? -ne 0 ]; then
     exit 13
 fi
@@ -712,12 +712,12 @@ fi
 # Sanity check
 # ------------
 
-sanity_check "$CFPR_MESA_BRANCH" "$CFPR_VK_GL_CTS_BRANCH"
+sanity_check "$CDLP_MESA_BRANCH" "$CDLP_VK_GL_CTS_BRANCH"
 if [ $? -ne 0 ]; then
     exit 2
 fi
 
-apply_verbosity "$CFPR_VERBOSITY"
+apply_verbosity "$CDLP_VERBOSITY"
 
 xhost +
 
